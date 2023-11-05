e0cb3214dadc28c5ae7388295dbde9c63e299793
Gianni Ioannou <108948908+gi6723@users.noreply.github.com>
2023-11-05 06:35:31 -0600

    Uploading images

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,9 @@
 from flask import Flask
-from flask_login import LoginManager
+#from flask_login import LoginManager
 from flask import Flask, render_template
-from app import routes
 
 app = Flask(__name__)
-login_manager = LoginManager(app)
 
-@app.route('/')
-
-def home():
-    return render_template('layout.html')
+from app import routes
+#login_manager = LoginManager(app)
 
